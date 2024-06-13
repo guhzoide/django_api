dc539cc5f0d66a1d80bceb4b9109e367044bbbf1
Hugo Ferreira <hugorobertiferreira61.br@gmail.com>
2024-06-13 17:48:36 -0300

    Curso em andamento
    Curso em andamento

--- apps/escola/urls.py
+++ apps/escola/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from apps.escola.views import alunos
+from django.urls import path, include
+from apps.escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
-    path('', alunos, name='alunos'),
+    path('', include(router.urls)),
 ]
--- apps/escola/views.py
+++ apps/escola/views.py
@@ -1,6 +1,16 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from apps.escola.models import Aluno, Curso
+from apps.escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Guilherme'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os Cursos"""
+
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
