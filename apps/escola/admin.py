6251536d2d87492cfd816e2edc7579b138324786
Hugo Ferreira <hugorobertiferreira61.br@gmail.com>
2024-06-16 12:29:34 -0300

    Finalizando primeiro modulo
    Finalizando primeiro modulo de API com Django 3: Django Rest Framework

--- apps/escola/admin.py
+++ apps/escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.escola.models import Aluno, Curso
+from apps.escola.models import Aluno, Curso, Matricula
 
 class ListandoAlunos(admin.ModelAdmin):
     list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
@@ -9,10 +9,17 @@ class ListandoAlunos(admin.ModelAdmin):
 
 admin.site.register(Aluno, ListandoAlunos)
 
-class ListandoCusros(admin.ModelAdmin):
+class ListandoCursos(admin.ModelAdmin):
     list_display = ('id', 'nome_curso', 'codigo_curso', 'descricao', 'nivel')
     list_display_links = ('id', 'nome_curso')
     search_fields = ('nome_curso', 'codigo_curso', 'nivel')
     list_per_page = 20
 
-admin.site.register(Curso, ListandoCusros)
+admin.site.register(Curso, ListandoCursos)
+
+class ListandoMatriculas(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id', 'aluno')
+    list_per_page = 20
+
+admin.site.register(Matricula, ListandoMatriculas)
--- apps/escola/views.py
+++ apps/escola/views.py
@@ -1,16 +1,50 @@
 from django.http import JsonResponse
-from rest_framework import viewsets
-from apps.escola.models import Aluno, Curso
-from apps.escola.serializer import AlunoSerializer, CursoSerializer
+from rest_framework import viewsets, generics
+from apps.escola.models import Aluno, Curso, Matricula
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from apps.escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculasSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
 
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os Cursos"""
 
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todos as Matriculas"""
+
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matriculas de um aluno(a)"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    
+    serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos(a) em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    
+    serializer_class = ListaAlunosMatriculasSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
